d04a857de184086666bad3363371b884eed75a1b
farrellb <farrellb@wit.edu>
2011-04-04 14:42:46 -0700

    first wave of configurations

---
+++ extract.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$ chmod +x extract.sh
+sudo  tar  zvxf opensips-1.6.4-2-tls_src.tar.gz
---
+++ installs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+$ chmod +x installs.sh
+sudo apt-get install bison bison++ bisonc++
+sudo apt-get install flex
+sudo apt-get install libsctp1
+sudo apt-get install mysql-server
+sudo apt-get install libmysqlclient-dev
+sudo apt-get install libxml2-dev
+sudo apt-get install libexpat1-dev
+sudo apt-get install libradius-ng2 libradius-ng-dev
+sudo apt-get install libcurl3-dev
+sudo apt-get install libxmlrpc-c3 libxmlrpc-c3-dev
+sudo apt-get install libperl-dev
+sudo apt-get install libsnmp-dev
+sudo apt-get install libconfuse0 libconfuse-dev
+sudo apt-get install build-essential
+sudo wget http://opensips.org/pub/opensips/1.6.4/src/opensips-1.6.4-2-tls_src.tar.gz 
+
+
+
+
+
+
